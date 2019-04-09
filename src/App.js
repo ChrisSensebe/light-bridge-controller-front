f91c85c1f8ace73ed9cf185592ad9171488ddf86
ChrisSensebe <christophe.sensebe@gmail.com>
2019-04-09 23:04:19 +0200

    using AppLight in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 import {AppTitle} from "./components/AppTitle";
+import {AppLights} from "./components/AppLights";
 
 class App extends Component {
   render() {
     return (
         <>
           <AppTitle title="Light bridge controller" />
+          <AppLights/>
         </>
     );
   }
