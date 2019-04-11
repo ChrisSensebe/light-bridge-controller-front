73aff22dab243a1794a5213910e792203f955b09
ChrisSensebe <christophe.sensebe@gmail.com>
2019-04-11 21:25:12 +0200

    uninstalling aphrodite and adding styled components

--- src/components/app-toggle.tsx
+++ src/components/app-toggle.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 interface AppToggleProps {
-    handleClick: (event: React.MouseEvent<HTMLElement>) => void;
+  handleClick: (event: React.MouseEvent<HTMLElement>) => void;
 }
 
 export const AppToggle = (props: AppToggleProps) => {
+
+  // const [checked, toggleCheck] = useState(false);
+
   return <label className={'switch'} onClick={props.handleClick}>
-      <input type="checkbox"/>
-      <span className={'slider'} />
+    <input type="checkbox" className={'input'}/>
+    <span className={'slider'} />
   </label>
 };
