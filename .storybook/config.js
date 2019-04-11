30e68c48d6b06e2e60545bf4f4f4052cf81eda37
ChrisSensebe <christophe.sensebe@gmail.com>
2019-04-11 23:34:27 +0200

    adding dark theme for storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,11 @@
-import { configure } from '@storybook/react';
+import { addParameters, configure } from '@storybook/react';
+import { themes } from '@storybook/theming';
+
+addParameters({
+  options: {
+    theme: themes.dark,
+  },
+});
 
 function loadStories() {
   require('../src/stories');
