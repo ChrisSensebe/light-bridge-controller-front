3807465ea781a2254853122d6ae986b3af0ef36a
ChrisSensebe <christophe.sensebe@gmail.com>
2019-04-12 23:24:02 +0200

    adding app-slider styles

--- src/components/app-slider.tsx
+++ src/components/app-slider.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 interface AppSliderProps {
   min: number;
@@ -7,10 +8,38 @@ interface AppSliderProps {
   onChangeHandler: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
+const Input = styled.input`
+  margin: 0;
+  -webkit-appearance: none;
+  width: 100%;
+  height: 15px;
+  border-radius: 5px;   
+  background: #d3d3d3;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+  :hover {
+    opacity: 1;
+  }
+  ::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 25px;
+    height: 25px;
+    border-radius: 50%; 
+    background: #4CAF50;
+    cursor: pointer;
+  }
+  ::-moz-range-thumb {
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    background: #4CAF50;
+    cursor: pointer;
+  }
+`;
+
 export const AppSlider = (props: AppSliderProps) => {
-  return (
-    <div>
-      <input type="range" min={props.min} max={props.max} value={props.value} onChange={props.onChangeHandler}/>
-    </div>
-  );
+  return <Input type="range" min={props.min} max={props.max} value={props.value} onChange={props.onChangeHandler}/>;
 };
